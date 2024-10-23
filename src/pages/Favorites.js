79aa707558856bba31564842e5984446c6f34d55
Fabián Gómez Campo <fgomezc@uxcale.com>
2024-10-23 13:51:39 +0200

    feat: favourite page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,24 @@
+import MeetupItem from "../components/meetups/MeetupItem";
+import Card from "../components/ui/Card";
+import { useFavourites } from "../providers/favourites-context/favourites-context-provider";
+import classes from "./../components/meetups/MeetupList.module.css";
+
+
 export default function FavoritesPage() {
+  const { favourites } = useFavourites();
+
   return (
     <section>
       <h1>Favorites Page</h1>
+      {favourites.length > 0 ? (
+        <ul className={classes.list} data-test="meet-up-item">
+          {favourites.map((meetup) => (
+            <MeetupItem key={meetup.id} data={meetup} />
+          ))}
+        </ul>
+      ) : (
+        <p>No favorites</p>
+      )}
     </section>
   );
 }

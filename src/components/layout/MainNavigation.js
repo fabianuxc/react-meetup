e6826951cb83b8b3966918160a5d5d02b03facbf
Fabián Gómez Campo <fgomezc@uxcale.com>
2024-10-23 12:05:12 +0200

    feat: navigation bar hides on scroll

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -2,9 +2,37 @@ import { ALL_MEETUP_PAGE, FAVORITES_PAGE, NEW_MEETUP_PAGE } from "./../../utils/
 
 import classes from "./MainNavigation.module.css";
 
+import React, { useState, useEffect} from "react";
+
+
 export default function MainNavigation({ setPage }) {
+  const [showHeader, setShowHeader] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+
+  useEffect(() => {
+
+    const handleHeaderScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        setShowHeader(false);
+      } else {
+        setShowHeader(true);
+      }
+  
+      setLastScrollY(window.scrollY);
+  
+    };
+    
+      window.addEventListener("scroll", handleHeaderScroll)
+
+      return () => {
+        window.removeEventListener("scroll", handleHeaderScroll)
+      }
+
+  }, [lastScrollY])
   return (
-    <header className={classes.header} data-test="navigation-header">
+    
+    <header className={`${classes.header} ${showHeader ? classes.show : classes.hide}`} data-test="navigation-header">
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
